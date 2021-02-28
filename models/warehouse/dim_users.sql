14db8e1be10cdad913729c360686297e31a3c8de
Loc Nguyen <vinhloc30796@gmail.com>
2021-02-28 22:18:59 +0000

    Modularize sources_users

--- models/warehouse/dim_users.sql
+++ models/warehouse/dim_users.sql
@@ -2,12 +2,17 @@
 {{ config(
     materialized='table',
     partition_by={
-      "field": "created_at",
-      "data_type": "timestamp"
+        "field": "created_at",
+        "data_type": "timestamp"
     } 
 ) }}
 
 
+with
+sources_users as (
+    select *
+    from {{ source('sources', 'users') }}
+)
 
-select *
-from {{ source('sources', 'users') }}
+select * 
+from sources_users
