662689ac1c64fbed7310d0f9d88f90df82228a33
Loc Nguyen <vinhloc30796@gmail.com>
2021-02-28 22:27:22 +0000

    Add stg_ga_sessions

--- models/warehouse/dim_users.sql
+++ models/warehouse/dim_users.sql
@@ -1,4 +1,3 @@
-
 {{ config(
     materialized='table',
     partition_by={
@@ -12,7 +11,27 @@ with
 sources_users as (
     select *
     from {{ source('sources', 'users') }}
+),
+
+stg_ga_sessions as (
+    select *
+    from {{ ref('stg_ga_sessions') }}
+),
+
+inter_ga_sessions_first_dates as (
+    -- Find the first session dates for each user
+    select
+        user_id,
+        min(date) as first_session_at,
+    from stg_ga_sessions
+    group by 1
+),
+
+users_attributions as (
+    select
+        *
+    from sources_users
 )
 
 select * 
-from sources_users
+from users_attributions
