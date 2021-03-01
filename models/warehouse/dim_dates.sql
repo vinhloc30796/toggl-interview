3f38eb8a9cdbb78b5875d5298eed8e803dc018a7
Loc Nguyen <vinhloc30796@gmail.com>
2021-03-01 02:32:01 +0000

    Add date_month

--- models/warehouse/dim_dates.sql
+++ models/warehouse/dim_dates.sql
@@ -1,6 +1,12 @@
-{{ dbt_utils.date_spine(
-    datepart="day",
-    start_date="cast('2020-01-01' as datetime)",
-    end_date="cast('2021-03-01' as datetime)"
-   )
-}}
+with days as (
+    {{ dbt_utils.date_spine(
+        datepart="day",
+        start_date="cast('2020-01-01' as datetime)",
+        end_date="cast('2021-03-01' as datetime)"
+    ) }}
+)
+
+select 
+    date_day,
+    datetime_trunc(date_day, month) as date_month,
+from days
